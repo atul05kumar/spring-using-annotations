9f0da2fbb4ed8a7039dd26f09cf2c4c9e42adf13
Atul Verma <atul05kumar@gmail.com>
2018-12-09 03:56:26 +0530

    Using propery file to set values of class variavles using annotation

--- src/com/atul/spring/CricketCoach.java
+++ src/com/atul/spring/CricketCoach.java
@@ -1,13 +1,17 @@
 package com.atul.spring;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component("myCricketCoach")
 public class CricketCoach implements Coach {
+	
+	@Value("${coach.ground}")
+	private String ground;
 
 	@Override
 	public String getDailyWorkout() {
-		return "Practice batting in the nets!";
+		return "Practice batting in " + ground + " ground!";
 	}
 
 	@Override
